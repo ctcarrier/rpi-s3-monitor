b74af75efa469bc4a90e78824b2ca8b10d21f11d
ctcarrier <ctcarrier@gmail.com>
2016-03-13 22:27:29 -0400

    Adding loop

--- monitor.py
+++ monitor.py
@@ -4,8 +4,7 @@ import os
 from os import listdir
 from os.path import isfile, join
 from config_persist import Persist
-from datetime import datetime
-from datetime import timedelta
+import time
 
 SETTINGS_FILE = "/var/lib/s3monitor/settings.cfg"
 LOG_FILENAME = '/var/log/s3monitor/s3monitor.log'
@@ -34,15 +33,17 @@ class App():
         toMonitor = persist.readLastConfig(SETTINGS_FILE)
         logging.info("Settings: " +str(toMonitor))
 
-        for monitor in toMonitor:
-            logging.info('Syncing ' +str(monitor['folder']))
-            logging.info('Synching %s to %s' % (monitor['folder'], monitor['bucket']))
-            for f in listdir(monitor['folder']):
-                logging.info(f)
-                if isfile(join(monitor['folder'], f)):
-                    logging.info('Going to upload %s' % f)
-                    with open(join(monitor['folder'], f),'rb') as file:
-                        self.conn.upload(f,file,monitor['bucket'])
+        while(True):
+            for monitor in toMonitor:
+                logging.info('Syncing ' +str(monitor['folder']))
+                logging.info('Synching %s to %s' % (monitor['folder'], monitor['bucket']))
+                for f in listdir(monitor['folder']):
+                    logging.info(f)
+                    if isfile(join(monitor['folder'], f)):
+                        logging.info('Going to upload %s' % f)
+                        with open(join(monitor['folder'], f),'rb') as file:
+                            self.conn.upload(f,file,monitor['bucket'])
+        time.sleep(1)
 
 if __name__ == "__main__":
     app = App().run()
